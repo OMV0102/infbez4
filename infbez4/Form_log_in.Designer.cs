c361d044f36e2dc5d1b6a6eea1e98298dc2c25dd
omv0102 <omv98@outlook.com>
2020-04-06 19:50:39 +0700

    Изменил название приложения

--- infbez4/Form_log_in.Designer.cs
+++ infbez4/Form_log_in.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txt_login = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txt_login
+            // 
+            this.txt_login.Location = new System.Drawing.Point(92, 75);
+            this.txt_login.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_login.Name = "txt_login";
+            this.txt_login.Size = new System.Drawing.Size(148, 26);
+            this.txt_login.TabIndex = 0;
             // 
             // Form_log_in
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(576, 281);
+            this.Controls.Add(this.txt_login);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
             this.Name = "Form_log_in";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BrowserApp: Вход";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_login;
     }
 }
--- infbez4/Program.cs
+++ infbez4/Program.cs
@@ -16,7 +16,7 @@ namespace infbez4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_log_in());
         }
     }
 }
